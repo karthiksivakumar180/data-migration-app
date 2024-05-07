90e39eded12e7527cedaf6cbfb2e31613ccf5259
karthiksivakumar180 <karthik.sivakumar@makoitlab.com>
2024-05-07 15:18:42 +0530

    Database ODBC driver added

---
+++ database.py
@@ -0,0 +1,45 @@
+import os
+import pyodbc
+
+# Load environment variables
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def connect_to_db():
+    # Define DB2 connection parameters
+    db_user = os.getenv("DB_USER")
+    db_password = os.getenv("DB_PASSWORD")
+    db_host = os.getenv("DB_HOST")
+    db_port = os.getenv("DB_PORT")
+    db_name = os.getenv("DB_NAME")
+
+    try:
+        # Establish DB2 connection
+        conn_str = f"DSN={db_host};DATABASE={db_name};UID={db_user};PWD={db_password}"
+        conn = pyodbc.connect(conn_str)
+        return conn
+    except pyodbc.Error as e:
+        print("Error connecting to DB2:", e)
+        return None
+
+    # try:
+    #     # Establish DB2 connection
+    #     conn = pyodbc.connect(dsn=dsn, user=user, password=password)
+    #     return conn
+    # except pyodbc.Error as e:
+    #     print("Error connecting to DB2:", e)
+    #     return None
+
+
+# def get_db_connection():
+#     db_user = os.getenv("DB_USER")
+#     db_password = os.getenv("DB_PASSWORD")
+#     db_host = os.getenv("DB_HOST")
+#     db_port = os.getenv("DB_PORT")
+#     db_name = os.getenv("DB_NAME")
+
+#     conn_str = f"DATABASE={db_name};HOSTNAME={db_host};PORT={db_port};PROTOCOL=TCPIP;UID={db_user};PWD={db_password};"
+#     conn = ibm_db.connect(conn_str, "", "")
+#     return conn
--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from fastapi import FastAPI
-import database
+from database import connect_to_db
+import pyodbc
 
 app = FastAPI()
 
+
 @app.get("/")
 async def read_root():
-    conn = database.get_db_connection()
-    # Execute database queries here
-    return {"message": "Hello World"}
+    print("route new")
+    # Establish database connection
+    conn = connect_to_db()
+    if conn:
+        try:
+            # Execute queries or perform database operations
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM your_table")
+            rows = cursor.fetchall()
+            result = [{"column1": row.column1, "column2": row.column2} for row in rows]
+            cursor.close()
+            conn.close()
+            return {"data": result}
+        except pyodbc.Error as e:
+            return {"error": str(e)}
+    else:
+        return {"error": "Failed to establish database connection"}
