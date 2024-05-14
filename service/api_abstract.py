5fdfdd26a83133877b23bdc6321f6f023e5e7cf1
karthiksivakumar180 <karthik.sivakumar@makoitlab.com>
2024-05-14 15:14:14 +0530

    API abstract with account api integration

---
+++ service/api_abstract.py
@@ -0,0 +1,30 @@
+import httpx
+
+#https://mrfcorplimited--oidev.sandbox.my.salesforce.com/services/oauth2/token?grant_type=password&client_id=3MVG9aWdXtdHRrI3od9Cx9LC9Y7nTApGqXnfphSTgD.1J32PcBMhMCDD5M0F_153HafFhv3nc0EDtOSIihDNS&client_secret=541A79EBCBED5FAFD6F29C4865528021EAF8C0EAF5F8B035B37689D2672C22CA&username=tthanabal@mrf.com.oidev&password=mrf@12345gsZek38J2PvOLyoKa2L7cTmRg
+
+
+#https://mrfcorplimited--oidev.sandbox.my.salesforce.com/services/data/v58.0/jobs/ingest/
+
+# {
+#     "object":"Account",
+#     "externalIdFieldName" :"EXT_ID__c",
+#     "contentType":"CSV",
+#     "operation":"upsert",
+#     "lineEnding":"CRLF"
+# }
+
+#https://mrfcorplimited--oidev.sandbox.my.salesforce.com/services/data/v58.0/jobs/ingest/750H1000005JEAcIAO/batches/
+
+# https://mrfcorplimited--oidev.sandbox.my.salesforce.com/services/data/v58.0/jobs/ingest/750H1000005JE4KIAW/
+
+# {
+#     "state": "UploadComplete"
+# }
+
+
+
+#https://mrfcorplimited--oidev.sandbox.my.salesforce.com/services/data/v56.0/jobs/ingest/750H1000005JEADIA4/successfulResults/
+#https://mrfcorplimited--oidev.sandbox.my.salesforce.com/services/data/v56.0/jobs/ingest/750H1000005JE4KIAW/failedResults/
+
+
+
