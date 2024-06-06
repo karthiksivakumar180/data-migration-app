e51bb0540fec58253367f30320f0614d427ce84f
karthiksivakumar180 <karthik.sivakumar@makoitlab.com>
2024-06-06 18:16:00 +0530

    Query service added

--- service/query_service.py
+++ service/query_service.py
@@ -1,10 +1,9 @@
 from database import connect_to_db
 
-account_query="select * from KNA1 a left join KNB1 b on a.KUNNR = b.KUNNR and b.BUKRS = 1101 left join KNVV c on a.KUNNR = c.KUNNR and c.VKORG = 1101"
+account_query = "select * from KNA1 a left join KNB1 b on a.KUNNR = b.KUNNR and b.BUKRS = 1101 left join KNVV c on a.KUNNR = c.KUNNR and c.VKORG = 1101"
 
 
 TABLE_REFERENCE = {
-    
     "sales_district": {"table": "T171", "view": "T171_SF", "where": None},
     "sales_group": {"table": "TVKGR", "view": "TVKGR_SF", "where": None},
     "plant": {"table": "T001W", "view": "T001W_SF", "where": "VKORG = '1101'"},
@@ -20,14 +19,18 @@ TABLE_REFERENCE = {
     "product_master_3": {"table": "MARC", "view": "", "where": "WERKS like 3*, 9*"},
     "customer_master_1": {"table": "KNA1", "view": "KNA1_SF", "where": None},
     "customer_master_2": {"table": "KNB1", "view": "KNB1_SF", "where": 'BUKRS ="1101"'},
-    "customer_master_3": {"table": "KNVV", "view": "KNVV_SF", "where": 'VKORG = "1101"'},
+    "customer_master_3": {
+        "table": "KNVV",
+        "view": "KNVV_SF",
+        "where": 'VKORG = "1101"',
+    },
     "changes": {"table": "CDHDR", "view": "CDHDR_SF", "where": 'TCODE ="XD02", "MM02"'},
     "billing": {
         "table": "VBRK",
         "view": "VBRK_SF",
         "where": 'VKORG = "1101" and FKDAT >= "01.04.2022"',
     },
-    "billing_item": {"table": "VBRP", "view": "VBRP_SF","where":None},
+    "billing_item": {"table": "VBRP", "view": "VBRP_SF", "where": None},
     "customer_cr_information": {
         "table": "KNKK",
         "view": "KNKK_SF",
@@ -75,6 +78,7 @@ TABLE_REFERENCE = {
 #         column_alias_mapping[col] = alias
 #     return column_alias_mapping
 
+
 def get_column_alias_mapping(query):
     # Extract columns and their aliases from the query
     column_alias_mapping = {}
@@ -83,10 +87,12 @@ def get_column_alias_mapping(query):
     select_part = query.lower().split("from")[0].replace("select", "").strip()
     for col_alias in select_part.split(","):
         col_alias = col_alias.strip()
-        
+
         # Split column and alias
         if " as " in col_alias:
             col, alias = [part.strip() for part in col_alias.split(" as ")]
+            if "." in col:
+                col = col.split(".")[-1]
         else:
             col = col_alias
             alias = col_alias.split(".")[-1]  # Default alias is the column name itself
@@ -95,13 +101,16 @@ def get_column_alias_mapping(query):
         original_alias = alias
         count = alias_count.get(alias, 0)
         while alias in column_alias_mapping.values():
+            print(alias_count)
             count += 1
             alias = f"{original_alias}_{count}"
+            # print("alias", alias)
         alias_count[original_alias] = count
 
-        column_alias_mapping[col] = alias
+        column_alias_mapping[alias] = col
 
     return column_alias_mapping
+
 
 def execute_dynamic_query(table_name, dynamic_where=None, page_number=1, page_size=10):
     conn = connect_to_db()
@@ -122,7 +131,7 @@ def execute_dynamic_query(table_name, dynamic_where=None, page_number=1, page_si
             rows = cursor.fetchall()
             # Extract column names
             columns = [column[0] for column in cursor.description]
-            
+
             result = [
                 {column: value for column, value in zip(columns, row)} for row in rows
             ]
